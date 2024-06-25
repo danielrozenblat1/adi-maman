5a8beff7e139436cdef4a2e84b68b961979868df
Your Name <you@example.com>
2024-06-25 13:14:23 +0300

    newFix

--- src/screens/FirstScreen.js
+++ src/screens/FirstScreen.js
@@ -6,7 +6,7 @@ const FirstScreen=()=>{
 
 return <>
 <div className={styles.description}>אם את רוצה סוף סוף לצאת לעצמאות, לעבוד בעבודה שאת אוהבת ,להיות הבוס של עצמך ולהרוויח בין 10,000-20,000₪ בחודש</div>
-<div className={styles.more}>ובשביל זה אני כאן</div>
+<div className={styles.more}>בשביל זה אני כאן</div>
 <div className={styles.center}><img className={styles.image} src={adi} alt="עדי ממן"/></div>
 <h1 className={styles.name}>עדי ממן</h1>
 <h2 className={styles.what}>אקדמיית בוטיק ללימוד ציפורניים</h2>
