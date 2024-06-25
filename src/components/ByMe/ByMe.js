7a6da4c25aada3325a067ba1203b030ccdf80389
Your Name <you@example.com>
2024-06-25 10:57:24 +0300

    newseo

--- src/components/ByMe/ByMe.js
+++ src/components/ByMe/ByMe.js
@@ -1,7 +1,5 @@
 import styles from "./ByMe.module.css"
 const ByMe=()=>{
-
-
     return <>
     <div className={styles.byMe}>נבנה על ידי <a  href="https://danielrozenblat.co.il/"  rel="noopener noreferrer" target="_blank" className={styles.a}>דניאל רוזנבלט</a></div>
     </>
